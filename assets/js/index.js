8381bced93c45b0b0812000273328c0b325e0e11
Min Htet Aung <minhtetaung672765@gmail.com>
2024-05-23 12:23:02 +0630

    up

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+
+// Make the page return to top when it's refreshed
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+};
+
+// This event handles the behaviour of header content
+// by manipulating class into the html, which create alternate styles by css
+document.addEventListener('DOMContentLoaded', function () {
+    var content_column = document.querySelector('.header-content .content-column');
+    var content_content = document.querySelector('.header-content .content-content');
+
+    content_column.classList.add('pageLoaded');
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 0) {
+            content_column.classList.remove('pageLoaded');
+            content_content.classList.add('scrolled');
+        } else {
+            content_column.classList.add('pageLoaded');
+            content_content.classList.remove('scrolled');
+        }
+
+    });
+});
--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,39 +1,18 @@
-
-// Make the page return to top when it's refreshed
-window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-};
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     var content_column = document.querySelector('.header-content .content-column');
-//     content_column.classList.add('pageLoaded');
-// });
-
 // This event handles the backgroundcolor adjustment of navigation bar
 // tracks the user action - scroll page - add new class to html element
 document.addEventListener('DOMContentLoaded', function () {
     var navbar = document.querySelector('.navbar');
-    var content_column = document.querySelector('.header-content .content-column');
-    var content_content = document.querySelector('.header-content .content-content');
-
-    content_column.classList.add('pageLoaded');
 
     window.addEventListener('scroll', function () {
         if (window.scrollY > 0) {
             navbar.classList.add('scrolled');
-            content_column.classList.remove('pageLoaded');
-            content_content.classList.add('scrolled');
         } else {
             navbar.classList.remove('scrolled');
-            content_column.classList.add('pageLoaded');
-            content_content.classList.remove('scrolled');
         }
-
         let previousScrollPoint = this.window.screenY;
         if (previousScrollPoint < this.window.screenY) {
             navbar.computedStyleMap('display : none');
         }
-
     });
 });
 
