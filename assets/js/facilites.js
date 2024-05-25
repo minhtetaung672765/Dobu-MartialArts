193f894bbf31f5dfb6e88b2f0511bb267a81c670
Min Htet Aung <minhtetaung672765@gmail.com>
2024-05-25 15:48:09 +0630

    up

---
+++ assets/js/facilites.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    // Function to check if an element is in the viewport
+    function isInViewport(element) {
+        var elementTop = $(element).offset().top;
+        var elementBottom = elementTop + $(element).outerHeight();
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    }
+
+    // Function to handle the scroll event
+    function onScroll() {
+        $('.animate-on-scroll').each(function () {
+            if (isInViewport(this) && !$(this).hasClass('in-view')) {
+                $(this).addClass('in-view');
+            }
+        });
+    }
+
+    // Bind the scroll and resize events to the onScroll function
+    $(window).on('scroll resize', onScroll);
+
+    // Trigger the onScroll function on page load
+    onScroll();
+});
