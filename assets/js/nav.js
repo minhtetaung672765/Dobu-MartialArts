47767a09441a356316377893e050526780c53546
Min Htet Aung <minhtetaung672765@gmail.com>
2024-05-27 17:33:58 +0630

    up

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -16,14 +16,36 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+// This event checks if the collapse-toggler is being clicked or not
 document.addEventListener('DOMContentLoaded', function () {
     var toggler = document.querySelector('.navbar .navbar-toggler');
     var navbar = document.querySelector('.navbar');
 
     toggler.addEventListener('click', function () {
-        navbar.classList.toggle('collapse-condition');
+        navbar.classList.toggle('collapse-toggler-focused');
     });
 });
+
+// This event checks if the navbar is in collapse condition or not (collapse-toggler shows up)
+$(document).ready(function () {
+    function checkNavbarCollapse() {
+        if ($(window).width() < 992) {
+            // 992px is the default Bootstrap breakpoint for navbar collapse
+            $('.navbar').addClass('collapse-condition');
+        } else {
+            $('.navbar').removeClass('collapse-condition');
+        }
+    }
+
+    // Initial check
+    checkNavbarCollapse();
+
+    // Check on window resize
+    $(window).resize(function () {
+        checkNavbarCollapse();
+    });
+});
+
 
 // This event handles monitoring the user's page scrolling behaviour 
 // tracks the user action - scroll page to down or not - add new class to html element
